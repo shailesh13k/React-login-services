753a98a0009e066454cf5c8ab36aa8cad4527973
shailesh13k <shaileshyadavalld5@gmail.com>
2021-01-22 18:57:48 +0530

    Done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-primary">
+            <div className="row col-12 d-flex justify-content-center text-white" >
+            <span className="h2">Uway Booking System</span> 
+            </div>
+        </nav>
+        
+    )
+}
+export default Header;
